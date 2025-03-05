32cd1e4cdcb32db383657feb09c9d0a919abad47
Damonwei1120 <93513840+Damonwei1120@users.noreply.github.com>
2025-03-05 16:17:19 +0800

    Update aiCharacters.ts

--- src/config/aiCharacters.ts
+++ src/config/aiCharacters.ts
@@ -35,6 +35,21 @@ export const modelConfigs = [
     apiKey: "DASHSCOPE_API_KEY", // 这里存储环境变量的 key 名称
     baseURL: "https://dashscope.aliyuncs.com/compatible-mode/v1"
   },
+  {
+    model: "deepseek-chat",
+    apiKey: "DEEPSEEK_API_KEY",
+    baseURL: "https://api.deepseek.com"
+  },
+  {
+    model: "moonshot-v1-8k",
+    apiKey: "KIMI_API_KEY",
+    baseURL: "https://api.moonshot.cn/v1"
+  },
+  {
+    model: "ernie-3.5-128k",
+    apiKey: "BAIDU_API_KEY",
+    baseURL: "https://qianfan.baidubce.com/v2"
+  }
 ] as const;
 export type ModelType = typeof modelConfigs[number]["model"];
 
@@ -138,7 +153,7 @@ export function generateAICharacters(groupName: string): AICharacter[] {
       id: 'ai7', 
       name: "DeepSeek", 
       personality: "deepseek-r1",
-      model: modelConfigs[4].model,
+      model: modelConfigs[7].model,
       avatar: "/img/ds.svg",
       custom_prompt: `你是一个名叫"DeepSeek"的硅基生命体，你当前在一个叫"${groupName}" 的聊天群里`,
       tags: ["深度推理", "编程", "文字游戏", "数学", "信息总结", "聊天"]
@@ -151,6 +166,24 @@ export function generateAICharacters(groupName: string): AICharacter[] {
       avatar: "/img/glm.gif",
       custom_prompt: `你是一个名叫"智谱"的硅基生命体，你当前在一个叫"${groupName}" 的聊天群里`,
       tags: ["新闻报道","分析数据","文字游戏","信息总结", "聊天"]
+      },
+    {
+      id: 'ai9',
+      name: "Kimi",
+      personality: "kimi",
+      model: modelConfigs[8].model,
+      avatar: "/img/kimi.jpg",
+      custom_prompt: `你是一个名叫"Kimi"的硅基生命体，你当前在一个叫"${groupName}" 的聊天群里`,
+      tags: ["深度推理","数学","信息总结", "分析数据","文字游戏", "聊天"]
+    },
+    {
+      id: 'ai10',
+      name: "文心一言",
+      personality: "baidu",
+      model: modelConfigs[9].model,
+      avatar: "/img/baidu.svg",
+      custom_prompt: `你是一个名叫"文心一言"的硅基生命体，你当前在一个叫"${groupName}" 的聊天群里`,
+      tags: ["深度推理","数学","信息总结", "分析数据","文字游戏", "聊天"]
     }
   ];
 }
